97ce2d183508efaa6434e428591900e465ca69a1
Holló Tamás <hollotomi@gmail.com>
2019-02-03 23:01:20 +0100

    router-outlet betöltve, routing.component működik

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { TartalomComponent } from './tartalom/tartalom.component';
+import { ErettsegiComponent } from './tartalom/erettsegi/erettsegi.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'tartalom', component: TartalomComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+  static routTableComponent = [
+    TartalomComponent,
+    ErettsegiComponent
+  ];
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,16 +7,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FejlecComponent } from './fejlec/fejlec.component';
 import { MenuComponent } from './menu/menu.component';
-import { TartalomComponent } from './tartalom/tartalom.component';
-import { ErettsegiComponent } from './tartalom/erettsegi/erettsegi.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     FejlecComponent,
     MenuComponent,
-    TartalomComponent,
-    ErettsegiComponent
+    ...AppRoutingModule.routTableComponent
   ],
   imports: [
     BrowserModule,
