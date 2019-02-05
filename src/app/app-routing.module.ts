1119bc2f8902c0f32f7e02e2720d445c4bcd842d
Holló Tamás <hollotomi@gmail.com>
2019-02-05 22:28:23 +0100

    A menü megértve és postig mélységében is rendben

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,21 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { TartalomComponent } from './tartalom/tartalom.component';
-import { ErettsegiComponent } from './tartalom/erettsegi/erettsegi.component';
-import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { ErettsegiComponent } from './erettsegi/erettsegi.component';
+import { ErettsegiTipusComponent } from './erettsegi-tipus/erettsegi-tipus.component';
+import { ErettsegiTipusListComponent } from './erettsegi-tipus-list/erettsegi-tipus-list.component';
+import { ErettsegiPostComponent } from './erettsegi-post/erettsegi-post.component';
 import { AboutComponent } from './about/about.component';
-import { ErettsegiTipusComponent } from './tartalom/erettsegi-tipus/erettsegi-tipus.component';
-import { ErettsegiTipusListComponent } from './tartalom/erettsegi-tipus-list/erettsegi-tipus-list.component';
-import { ErettsegiPostComponent } from './tartalom/erettsegi-post/erettsegi-post.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 const routes: Routes = [
   {path: 'tartalom', component: TartalomComponent},
-  {path: 'erettsegi',
-   component: ErettsegiComponent,
+  {path: 'erettsegi', component: ErettsegiComponent,
    children: [
-     {path: 'erettsegi/:tipus', component: 'ErettsegiTipusComponent'},
-     {path: 'erettsegi/:tipus/list', component: 'ErettsegiTipusListComponent'},
-     {path: 'erettsegi/:tipus/:post', component: 'ErettsegiPostComponent'}
+     {path: 'tipus', component: ErettsegiTipusComponent},
+     {path: ':tipusID/list', component: ErettsegiTipusListComponent},
+     {path: ':tipusID/:postID/post', component: ErettsegiPostComponent}
    ]},
   {path: 'about', component: AboutComponent},
   {path: '', redirectTo: '/tartalom', pathMatch: 'full' },
@@ -30,10 +29,10 @@ export class AppRoutingModule {
   static routTableComponent = [
     TartalomComponent,
     ErettsegiComponent,
-    AboutComponent,
     ErettsegiTipusComponent,
     ErettsegiTipusListComponent,
     ErettsegiPostComponent,
+    AboutComponent,
     PageNotFoundComponent
   ];
 }
