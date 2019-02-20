d55a84d3f5a74fcf034eba1988694837e34d4d60
Holló Tamás <hollotomi@gmail.com>
2019-02-20 15:13:09 +0100

    Location a guard-ba

--- src/app/shared/logged-in.guard.ts
+++ src/app/shared/logged-in.guard.ts
@@ -3,13 +3,15 @@ import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angul
 import { Observable } from 'rxjs';
 import { UserService } from './user.service';
 import { Router } from '@angular/router';
+import { Location } from '@angular/common';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LoggedInGuard implements CanActivate {
   constructor(private _userService: UserService,
-              private _router: Router ) {
+              private _router: Router,
+              private _location: Location) {
 
   }
 
@@ -19,8 +21,8 @@ export class LoggedInGuard implements CanActivate {
     if (this._userService.isLoggedIn) {
       return true;
     } else {
-      this._router.navigate(['/tartalom']);
+      this._router.navigate([this._location.path()]);
+      return false;
     }
-
   }
 }
